428688bab131d835723bd9ef07bccc9e58051bb3
mjklienst <mklienstuber@mail.wou.edu>
2020-01-23 11:51:30 -0800

    Part 1 of milestone 3, created project

---
+++ Milestone 3/ThrowawayProject/ThrowawayProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThrowawayProject.Startup))]
+namespace ThrowawayProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
