8731e51c0aa7b72ef277d1972a193e9ac9a3d1b2
mjklienst <mklienstuber@mail.wou.edu>
2020-01-30 18:05:33 -0800

    Changed bases to AzureConnection so we can access db online

--- Milestone 3/ThrowawayProject/ThrowawayProject/DAL/AthleteContext.cs
+++ Milestone 3/ThrowawayProject/ThrowawayProject/DAL/AthleteContext.cs
@@ -11,7 +11,7 @@ namespace ThrowawayProject.DAL
     public partial class AthleteContext : DbContext
     {
         public AthleteContext()
-            : base("name=AthleteContext")
+            : base("name=AzureConnection")
         {
         }
 
