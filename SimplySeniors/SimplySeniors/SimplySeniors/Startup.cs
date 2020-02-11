0861a5f150692766f60a8e768e1e8d88080dd40a
mjklienst <mklienstuber@mail.wou.edu>
2020-02-10 18:54:10 -0800

    Adding starting project for Simply Seniors, created up and down scripts and initial project

---
+++ SimplySeniors/SimplySeniors/SimplySeniors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimplySeniors.Startup))]
+namespace SimplySeniors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
