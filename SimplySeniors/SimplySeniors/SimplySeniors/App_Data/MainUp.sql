6e952664f76e595b6ae5819e642a6f0bffb1618c
jgillespie <jgillespie17@wou.edu>
2020-02-18 20:51:30 -0800

    Added new fields to upscript, changed layout of details page to look 'better'

--- SimplySeniors/SimplySeniors/SimplySeniors/App_Data/MainUp.sql
+++ SimplySeniors/SimplySeniors/SimplySeniors/App_Data/MainUp.sql
@@ -6,17 +6,24 @@
 	[BIRTHDAY] DATE NOT NULL,
 	[LOCATION] NVARCHAR (128) NOT NULL,
 	[VETSTATUS] BIT,
+	[OCCUPATION] NVARCHAR (128) NOT NULL,
+	[FAMILY] NVARCHAR (128), /*Probably needs to make a new table if we want to add feature*/ 
 	[BIO] NVARCHAR (2048)
 	CONSTRAINT [PK_dbo.Profiles] PRIMARY KEY CLUSTERED ([ID] ASC)
+	/**
+	FOR FUTURE USE PROBABLY
+	CONSTRAINT [FK_dbo.Profiles_dbo.Friends_FriendsID] FOREIGN KEY ([FriendsID]) REFERENCES dbo.Friends ([ID])
+	CONSTRAINT [FK_dbo.Profiles_dbo.Groups_GroupID] FOREIGN KEY ([GroupID]) REFERENCES dbo.Groups ([ID])
+	**/
 );
 
 
 /****** SEED DATA ******/
 
-INSERT INTO [dbo].[Profile] (FIRSTNAME, LASTNAME, BIRTHDAY, LOCATION, VETSTATUS, BIO) VALUES
-	('Chris', 'Columbus', '1451-09-01', 'Bahamas', 0, 'I discovered india guys i swear'),
-	('Ferdinand', 'Magellan', '1480-02-03', 'Chile', 0, 'I will go around the world guys srsly'),
-	('Juan', 'de Fuca', '1536-02-11', 'Vancouver Island', 0, 'I found a really rainy place' ),
-	('Leif', 'Eriksson', '0970-11-15', 'Vinland', 1, 'I found Canada, pretty cool eh?' ),
-	('Marco', 'Polo', '1254-01-08', 'Peking', 0, 'Mongolia rough yo'),
-	('Sacagawea', 'Shoshone', '1788-05-01', 'Astoria', 0, 'I led dudes accross the United States')
+INSERT INTO [dbo].[Profile] (FIRSTNAME, LASTNAME, BIRTHDAY, LOCATION, VETSTATUS, OCCUPATION, FAMILY, BIO) VALUES
+	('Chris', 'Columbus', '1451-09-01', 'Bahamas', 0, 'Explorer', 'Chris Jr.' ,'I discovered india guys i swear'),
+	('Ferdinand', 'Magellan', '1480-02-03', 'Chile', 0, 'Explorer','Ferdinand Jr.', 'I will go around the world guys srsly'),
+	('Juan', 'de Fuca', '1536-02-11', 'Vancouver Island', 0, 'Explorer', 'Juan Jr.', 'I found a really rainy place' ),
+	('Leif', 'Eriksson', '0970-11-15', 'Vinland', 1, 'Viking turned Explorer','Leif Liefsson', 'I found Canada, pretty cool eh?' ),
+	('Marco', 'Polo', '1254-01-08', 'Peking', 0, 'Explorer turned pool game', 'MARCO! POLO!', 'Mongolia rough yo'),
+	('Sacagawea', 'Shoshone', '1788-05-01', 'Astoria', 0, 'Actual Explorer', 'Sacagawea but on a coin', 'I led dudes accross the United States')
