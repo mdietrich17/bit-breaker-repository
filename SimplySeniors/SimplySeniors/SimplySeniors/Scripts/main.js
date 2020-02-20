cad35c607b169dd6ca6b12552414122316772abc
mjklienst <mklienstuber@mail.wou.edu>
2020-02-20 10:41:54 -0800

    Finished help page and added links to diff sections of website

--- SimplySeniors/SimplySeniors/SimplySeniors/Scripts/main.js
+++ SimplySeniors/SimplySeniors/SimplySeniors/Scripts/main.js
@@ -2,51 +2,51 @@
     $('#CreateAcc').on('click', function accInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3 id="descTitle">How to Register</h3><br /><p>Select "Register" on the navigation bar at the top of this webpage, on the right-hand side. You will then be prompted to type in your email address, which you will use to login to your account later. You will also be prompted to type in a password, which consists of at least 6 characters (with at least one character being a digit or non-letter, and at least one uppercase letter). You should then be able to register by selecting "Register". </p><br /><h3 id="descTitle">How to Log In</h3><br /><p>Select "Log in" on the navigation bar at the top of this webpage, on the right-hand side. You will then be prompted to type in your email address (the email address you used to register with). You will then be prompted to type in your password (the password you used to register with). Selecting "Remember me?" will let your computer remember your username and password, so anytime you enter the login page on <i>that computer,</i> your username and password will be remembered, so logging in will be as easy as clicking "Log in". </p>'));
+        $('#description').append($('<h3 id="descTitle">How to Register</h3><br /><p>Select "Register" on the navigation bar at the top of this webpage or <a href="/Account/Register">Click here</a>. You will then be prompted to type in your email address, which you will use to login to your account later. You will also be prompted to type in a password, which consists of at least 6 characters (with at least one character being a digit or non-letter, and at least one uppercase letter). You should then be able to register by selecting "Register". You will then be prompted to verify your email address. Simply do this by going to your email and clicking the confirmation link sent to your email address. You will need to have your account confirmed in order to recover your password if you later forget it. </p><br /><h3 id="descTitle">How to Log In</h3><br /><p>Select "Log in" on the navigation bar at the top of this webpage or <a href="/Account/Login">Click here</a>. You will then be prompted to type in your email address (the email address you used to register with). You will then be prompted to type in your password (the password you used to register with). Selecting "Remember me?" will let your computer remember your username and password, so anytime you enter the login page on <i>that computer,</i> your username and password will be remembered, so logging in will be as easy as clicking "Log in". </p> <br /><h3 id="descTitle">Recover your Password</h3><br /><p>If you ever forget your password, select "Log In" at the top of the navigation bar, and then select "Forgot your Password?". You will then be prompted to type in your email address (the one you registered with), and then click "Email Link". You will then be sent an email from us with a link you need to click on in able to reset your password. After the reset is completed, the new password you entered will now be your current password unless you forget it in the future. Tip: Write this password down so future you will remember! </p>'));
     });
     $('#HomePage').on('click', function homeInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>About Your Home Page</h3><p>*Update when homepage feature gets added*</p>'));
+        $('#description').append($('<h3 id="descTitle">About Your Home Page</h3><br /><p>*Update when homepage feature gets added*</p>'));
     });
     $('#ProfilePage').on('click', function profInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>About Your Profile Page</h3><p>*Update when profile feature gets added*</p>'));
+        $('#description').append($('<h3 id="descTitle">About Your Profile Page</h3><br /><p>*Update when profile feature gets added*</p> <br/> <h3 id="descTitle">Searching For Other Profile Pages</h3><br /><p>To search for a profile page, <a href="/Profiles">Click here</a>. You can search by first name, last name, or parts of a name. Once you find someone you know, or someone you would like to know, select "See Profile" and you will be redirected to their profile page. </p>'));
     });
     $('#Friend').on('click', function friendInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>How to Add Friends</h3><p>*Update when Friends feature gets added*</p>'));
+        $('#description').append($('<h3 id="descTitle">How to Add Friends</h3><br /><p>*Update when Friends feature gets added*</p>'));
     });
     $('#Messaging').on('click', function messageInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($("<h3>How to Send Messages</h3><p>*Update when Message feature gets added*</p>"));
+        $('#description').append($('<h3 id="descTitle">How to Send Messages</h3><br /><p>*Update when Message feature gets added*</p>'));
     });
     $('#Services').on('click', function serviceInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>About Services Page</h3><p>*Update when Service page gets added*</p>'));
+        $('#description').append($('<h3 id="descTitle">About Services Page</h3><br /><p>Our Services Page consists of many different types of services that may benefit you or those around you. To learn about what services may benefit you, <a href="/Home/Services">Click here</a>.</p>'));
     });
     $('#Events').on('click', function eventInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>About Events Page</h3><p>*Update when Events feature/page gets added*</p>'));
+        $('#description').append($('<h3 id="descTitle">About Events Page</h3><br /><p>*Update when Events feature/page gets added*</p>'));
     });
     $('#FAQ').on('click', function FAQInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<ol id="FAQList"> <li> Question 1? </li > <p>Answer</p> <li>Question 2?</li> <p>Another Answer</p> <li>Question 3?</li> <p>A Third Answer</p> </ol >  '));
+        $('#description').append($('<h3 id="descTitle">Frequently Asked Questions</h3> <br /> <ol id="FAQList"> <li> How much does this service cost? </li > <p>This service is, and always will be, completely free for the benefit of our users. We may eventually have advertisements displayed, but those will also be beneficial to you and your experience with our website, as they will be advertisements about helpful things that pertain to you or your location. </p> <li>How does "Find Friends" work and how does it suggest friends to me?</li> <p>As of right now, the "Find Friends" feature is not currently implemented, so we are unsure of how you will find friends. </p> <li>How do I change my privacy settings, and who can see my posts and profile information?</li> <p>As of right now, anyone can see your profile information just by searching for you on the website. In the future, we will add security features so that you can choose who can see your profile and your posts.</p> <li>How can I change my notification settings?</li><p>As of right now, we do not have notifications up, this will be added later.</p> <li>How long are my messages available when I am messaging friends?</li><p>As of right now, we do not have messaging implemented. Please check back later.</p> </ol >  '));
     });
     $('#Contact').on('click', function contactInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>Contact Us</h3><p>*Update when Contact feature gets added. Add email box here*</p>'));
+        $('#description').append($('<h3 id="descTitle">Contact Us</h3><br /><p>*Update when Contact feature gets added. Add email box here*</p>'));
     });
     $('#Vids').on('click', function vidInfo() {
         document.getElementById("description").remove();
         $('#descriptionOutside').append($('<div id="description"></div> '));
-        $('#description').append($('<h3>Help Videos</h3><p>*Update when we find helpful videos*</p>'));
+        $('#description').append($('<h3 id="descTitle">Help Videos</h3><br /><a href="https://www.youtube.com/watch?v=QBBzE6PITks">Self Defense for Seniors and How to Keep Yourself Safe</a><br/> <a href="https://www.youtube.com/watch?v=MWoS9oFJIzY">How to Create a Life of Purpose for Seniors</a><br/>  <a href="https://www.youtube.com/watch?v=JejTelL05Qw">Strength Training for Seniors</a><br/> '));
     });
 });
