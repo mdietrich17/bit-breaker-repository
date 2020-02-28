7e87771b06a29bbd5f4a1fb40798f0d032cf09c9
Dennis Ch <dcherepanov18@mail.wou.edu>
2020-02-27 18:13:46 -0800

    Minor changes to profile table, need crud and scafolding

--- SimplySeniors/SimplySeniors/SimplySeniors/App_Data/MainDown.sql
+++ SimplySeniors/SimplySeniors/SimplySeniors/App_Data/MainDown.sql
@@ -1,6 +1,7 @@
 ﻿
 ALTER TABLE [dbo].[HobbyBridge] DROP CONSTRAINT [FK_dbo.HobbyBridge_dbo.Hobbies_ID]
 ALTER TABLE [dbo].[HobbyBridge] DROP CONSTRAINT [FK_dbo.HobbyBridge_dbo.Profile_ID]
+ALTER TABLE [dbo].[Profile] DROP CONSTRAINT [FK_dbo.Profile_dbo.AspNetUsers_ID]
 
 
 DROP TABLE [dbo].[Profile]
