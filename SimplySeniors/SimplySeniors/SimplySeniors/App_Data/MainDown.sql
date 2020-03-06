925e368e2d8ff131c54b06cf71994e03a381558b
jgillespie <jgillespie17@wou.edu>
2020-03-05 18:42:26 -0800

    tried to find fixes to website errors

--- SimplySeniors/SimplySeniors/SimplySeniors/App_Data/MainDown.sql
+++ SimplySeniors/SimplySeniors/SimplySeniors/App_Data/MainDown.sql
@@ -2,7 +2,7 @@
 ALTER TABLE [dbo].[HobbyBridge] DROP CONSTRAINT [FK_dbo.HobbyBridge_dbo.Hobbies_ID]
 ALTER TABLE [dbo].[HobbyBridge] DROP CONSTRAINT [FK_dbo.HobbyBridge_dbo.Profile_ID]
 ALTER TABLE [dbo].[Posts] DROP CONSTRAINT [FK_dbo.Posts_dbo.Profile_ID]
-ALTER TABLE [dbo].[Profile] DROP CONSTRAINT [FK_dbo.Profile_dbo.AspNetUsers_ID]
+--ALTER TABLE [dbo].[Profile] DROP CONSTRAINT [FK_dbo.Profile_dbo.AspNetUsers_ID]
 
 DROP TABLE [dbo].[Profile]
 DROP TABLE [dbo].[Hobbies]
