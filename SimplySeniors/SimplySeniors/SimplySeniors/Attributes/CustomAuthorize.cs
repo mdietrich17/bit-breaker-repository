86fb7633bd8b5998edcaf8a8526b332ca36d2234
unknown <dcherepanov18@mail.wou.edu>
2020-04-03 09:06:27 -0700

    edited db and model to inlude bit, started working on authorize attribute

---
+++ SimplySeniors/SimplySeniors/SimplySeniors/Attributes/CustomAuthorize.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimplySeniors;
+
+namespace SimplySeniors.Attributes
+{
+
+public class AuthorizeAdminOrOwnerOfPostAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var authorized = base.AuthorizeCore(httpContext);
+            if (!authorized)
+            {
+                // The user is not authenticated
+                return false;
+            }
+
+            var user = httpContext.User;
+
+            var rd = httpContext.Request.RequestContext.RouteData;
+            var id = rd.Values["id"] as string;
+            if (string.IsNullOrEmpty(id))
+            {
+                // No id was specified => we do not allow access
+                return false;
+            }
+
+            return IsOwnerOfPost(user.Identity.Name, id);
+        }
+
+    }
+}
