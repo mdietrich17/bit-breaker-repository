04c53f608a828e4e0e6eb9c7067f836ad5995556
jgillespie <jgillespie17@wou.edu>
2020-04-06 15:26:17 -0700

    Added more buttons to allow for easier movement between different profile pages

--- SimplySeniors/SimplySeniors/SimplySeniors/Controllers/ProfilesController.cs
+++ SimplySeniors/SimplySeniors/SimplySeniors/Controllers/ProfilesController.cs
@@ -116,7 +116,7 @@ namespace SimplySeniors.Controllers
                 profile.PROFILECREATED = true; //user created profile
                 db.Profiles.Add(profile);
                 db.SaveChanges();
-                return RedirectToAction("HomePage","UserHomePage"); //edit this line maddy
+                return RedirectToAction("Create","HobbyBridges"); //edit this line maddy
             }
 
             return View(profile);
@@ -143,13 +143,14 @@ namespace SimplySeniors.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "FIRSTNAME,LASTNAME,BIRTHDAY,LOCATION,VETSTATUS,OCCUPATION,FAMILY,BIO")] Profile profile)
+        public ActionResult Edit([Bind(Exclude = "USERID", Include = "FIRSTNAME,LASTNAME,BIRTHDAY,LOCATION,VETSTATUS,OCCUPATION,FAMILY,BIO")] Profile profile)
         {
+            ModelState.Remove("USERID");
             if (ModelState.IsValid)
             {
                 db.Entry(profile).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Details", new { id = profile.ID });
             }
             return View(profile);
         }
