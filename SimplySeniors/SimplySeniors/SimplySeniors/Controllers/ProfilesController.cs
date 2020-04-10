a39a223b16192588674abca0b64f6cab8bfb2f33
mjklienst <mklienstuber@mail.wou.edu>
2020-04-10 10:15:09 -0700

    modified LINQ query in ProfilesController by adding several LINQ queries that search based on both first and last name, and not just one or the other

--- SimplySeniors/SimplySeniors/SimplySeniors/Controllers/ProfilesController.cs
+++ SimplySeniors/SimplySeniors/SimplySeniors/Controllers/ProfilesController.cs
@@ -40,7 +40,7 @@ namespace SimplySeniors.Controllers
 
                 if (!String.IsNullOrEmpty(searchString))
                 {
-                    products = products.Where(s => s.LASTNAME.Contains(searchString) || s.FIRSTNAME.Contains(searchString));   // Searching for matches through last name. 
+                    products = products.Where(s => s.LASTNAME.Contains(searchString) || s.FIRSTNAME.Contains(searchString) || (s.FIRSTNAME + " " + s.LASTNAME).Contains(searchString) || (s.LASTNAME + " " + s.FIRSTNAME).Contains(searchString) || (s.LASTNAME + ", " + s.FIRSTNAME).Contains(searchString));   // Searching for matches through last name. 
                 }
                 return View(products.ToList());
             }
