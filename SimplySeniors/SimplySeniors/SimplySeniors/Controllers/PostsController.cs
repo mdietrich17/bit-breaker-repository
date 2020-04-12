85df98cff2f37166d880756c71b4d3842fa8c745
Dennis Ch <dcherepanov18@mail.wou.edu>
2020-04-12 12:54:49 -0700

    attempted to fix conflicts

--- SimplySeniors/SimplySeniors/SimplySeniors/Controllers/PostsController.cs
+++ SimplySeniors/SimplySeniors/SimplySeniors/Controllers/PostsController.cs
@@ -86,7 +86,7 @@ namespace SimplySeniors.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,Body,ProfileID")] Post post)
+        public ActionResult Edit([Bind(Include = "ID,Title,Body")] Post post)
         {
             if (ModelState.IsValid)
             {
