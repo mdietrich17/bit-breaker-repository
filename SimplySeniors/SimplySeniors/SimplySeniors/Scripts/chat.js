1e1bdea82cfb38a038aee0cd1134ca655dfe8b3d
Mike Dietrich <mdietrich17@wou.edu>
2020-05-27 20:40:55 -0700

    added group chat to homepage, open to everyone and moderated by the admin account, tried to test best I could locally

--- SimplySeniors/SimplySeniors/SimplySeniors/Scripts/chat.js
+++ SimplySeniors/SimplySeniors/SimplySeniors/Scripts/chat.js
@@ -1,124 +1,74 @@
-﻿////Ajax call to get JSON information for page to be responsive.
-//function AjaxCall(url, data, type) {
-//    return $.ajax({
-//        url: url,
-//        type: type ? type : 'GET',
-//        data: data,
-//        contentType: 'application/json'
-//    });
-//}
-////Throwing error if there was something wrong with the ajax call.
-//function ajaxError() {
-//    console.log('Error on AJAX return, check your scripts');
-//}
-
-
-
-$(function () {
-
-    var a = document.getElementById("firstNameBox").value;
-
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "/Chat/GetCurrentUser",
-        data: {},
-        success: displayMember,
-        error: errorOnAjax
-    });
-
-    function displayMember() {
-
-        $("#memberDDL").html("h");
-        var selectOptions = "";
-        selectOptions += '<option value="Select">Select</option>';
-        for (var i = 0; i < response.length; i++) {
-            selectOptions += '<option value="' + response[i] + '">' + response[i] + "</option>";
-        }
-        $("#memberDDL").append(selectOptions);
-
-    }
-});
-
-//$(function () {
-//    var url = "/Chat/GetAllProfiles";
-
-//    AjaxCall(url, null).done(function(response) {
-//        if (response.length > 0) {
-//            $('#memberDDL').html('h');
-//            var selectOptions = '';
-//            selectOptions += '<option value="Select">Select</option>';
-//            for (var i = 0; i < response.length; i++) {
-//                selectOptions += '<option value="' + response[i] + '">' + response[i] + '</option>';
-//            }
-//            $('#memberDDL').append(selectOptions);
-//        }
-//    }).fail(function(error) {
-//        alert("Get All Profiles() had an error check userHomePage controller");
-//    });
-//});
-
-
-//$('#memberDDL').on("change", function () {
-//    var teamName = $('#memberDDL').val();
-//    var object = { team: teamName };
-//    AjaxCall('/Chat/GetAllProfiles', JSON.stringify(object), 'POST').done(function (response) {
-//        if (response.length > 0) {
-//            $('#memberDDL').html('');
-//            var selectOptions = '';
-//            selectOptions += '<option value="Select">Select</option>';
-//            for (var responseStart = 0; responseStart < response.length; responseStart++) {
-//                selectOptions += '<option value="' + response[responseStart] + '">' + response[responseStart] + '</option>';
-//            }
-//            $('#memberDDL').append(selectOptions);
-//        }
-//    }).fail(function (error) {
-//        alert("Error2, Contact your nearest Help Desk"); //error2 output
-//    });
-//});
-
-
-
-(function (t, a, l, k, j, s) { // Adding talk.js dynamically via js so it doesn't delay page. 
-    s = a.createElement('script'); s.async = 1; s.src = "https://cdn.talkjs.com/talk.js"; a.head.appendChild(s)
-        ; k = t.Promise; t.Talk = {
-        v: 3, ready: {
-            then: function (f) {
-                if (k) return new k(function (r, e) { l.push([f, r, e]) }); l
-                    .push([f])
-            }, catch: function () { return k && new k() }, c: l
-        }
-    };
+﻿(function (t, a, l, k, j, s) {
+        s = a.createElement('script'); s.async = 1; s.src = "https://cdn.talkjs.com/talk.js"; a.getElementsByTagName('head')[0].appendChild(s); k = t.Promise
+        t.Talk = {ready: {then: function (f) { if (k) return new k(function (r, e) {l.push([f, r, e])}); l.push([f]) }, catch: function () { return k && new k() }, c: l } }
 })(window, document, []);
 
-Talk.ready.then(function () {
-    var me = new Talk.User({
-        id: "123456",
-        name: "Alice",
-        email: "alice@example.com",
-        photoUrl: "https://demo.talkjs.com/img/alice.jpg",
-        welcomeMessage: "Hey there! How are you? :-)"
-    });
-    var appID = System.Web.Configuration.WebConfigurationManager.AppSettings["chatApiKey"];
 
-    window.talkSession = new Talk.Session({
-        appId: appID,
+Talk.ready.then(function () {
+        // The core TalkJS lib has loaded, so let's identify the current user to TalkJS.
+        var firstName = document.getElementById("userFirstName").value;
+        var lastName = document.getElementById("userLastName").value;
+        var userId = document.getElementById("userID").value;
+        var me = new Talk.User({
+            // must be any value that uniquely identifies this user
+            id: userId,
+        name: firstName + " " + lastName,
+        email: "george@looney.net",
+        photoUrl: "https://talkjs.com/docs/img/george.jpg"
+    });
+    // TODO: add a "configuration" field to the user object so your
+    // user can get email notifications.
+    // See https://talkjs.com/docs/Emails_and_Configurations.html for more
+    // info.
+
+    //var appID = System.Web.Configuration.WebConfigurationManager.AppSettings["chatApiKey"];  // SWAP OUT FOR API KEY AFTER TESTING. 
+        window.talkSession = new Talk.Session({
+        appId: "tnDbD1Bo",
         me: me
     });
 
-    var other = new Talk.User({
-        id: "654321",
-        name: "Sebastian",
-        email: "Sebastian@example.com",
-        photoUrl: "https://demo.talkjs.com/img/sebastian.jpg",
-        welcomeMessage: "Hey, how can I help?"
-    });
+    // Let's show the chatbox.
+    // tnDbD1Bo
+    // First, we need to define who we want to talk to.
+    //
+    // In this case, it's always the operator. The code below is identical
+    // to the `var me =` declaration in operator.html
 
-    
-    var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other));
-    conversation.setParticipant(me);
-    conversation.setParticipant(other);
-    var inbox = talkSession.createInbox({ selected: conversation });
-    inbox.mount(document.getElementById("talkjs-container"));
+
+    //var firstName2 = document.getElementById("userFirstName2").value;   // using these for direct chat. 
+    //var lastName2 = document.getElementById("userLastName2").value;
+    //var userId2 = document.getElementById("userID2").value;
+        var operator = new Talk.User({
+            // just hardcode any user id, as long as your real users don't have this id
+            id: "99976",
+            name: "Admin",
+            email: "teambitbreakers@gmail.com",
+            photoUrl: "/Photos/favicon-32x32.png",
+            welcomeMessage: "Welcome to Simply Seniors, chat away!"
 });
+
+// Now, let's start or continue the conversation with the operator and
+// show the chatbox.
+
+// You control the ID of a conversation. In this example, we use the item ID as
+// the conversation ID in order to tie this conversation to this item.
+var conversation = window.talkSession.getOrCreateConversation("first_group_chat");
+conversation.setParticipant(me);
+conversation.setParticipant(operator);
+var chatbox = window.talkSession.createChatbox(conversation);
+chatbox.mount(document.getElementById("talkjs-container"));
+
+
+// Below is code for direct chat between two users. 
+//var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other));
+//conversation.setParticipant(me);
+//conversation.setParticipant(other);
+//var inbox = talkSession.createInbox({selected: conversation });
+//inbox.mount(document.getElementById("talkjs-container"));
+
+});
+
+
+
+
 
